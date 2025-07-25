cdf2a9d82b19b935b9bef15581f961e07a174bd6
nexiuslabs <nexiuslabs@gmail.com>
2025-07-26 00:17:41 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-typescript'
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
