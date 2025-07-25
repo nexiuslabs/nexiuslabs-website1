a3747bb5bed4e28e26786b03d1d4f7756f5e213c
nexiuslabs <nexiuslabs@gmail.com>
2025-07-25 13:20:36 +0700

    Convert Tailwind Config to CommonJS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,7 @@
-```javascript
-import typography from '@tailwindcss/typography';
+const typography = require('@tailwindcss/typography');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -30,4 +29,3 @@ export default {
     typography
   ],
 }
-```
